1f511b94b9dcf2457b41fe9e8b2eb583db3e5275
Nodir <makhmudovnodir111gmail.com>
2023-01-23 22:52:16 +0500

    admin

---
+++ front/src/components/NewsItem.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="wrapper">
+    <main class="section">
+      <div class="page__wrapper">
+        <div class="page__screen screen">
+          <section class="page_link">
+            <div class="container">
+              <div class="page_link-inner">
+                <a href="index.html">Главная /</a>
+                <a href="news.html">Новости /</a>
+                <a href="news.html">В школе...</a>
+              </div>
+            </div>
+          </section>
+          <section class="page__content">
+            <div class="container">
+              <div class="page__flex-column">
+                <div class="more-title">
+                  <h1>Lorem ipsum dolor sit.</h1>
+                  <span>02.06.2022</span>
+                </div>
+                <div class="more">
+                  <div class="more-img-block">
+                    <img
+                      src="/images/111079647.jpg"
+                      alt=""
+                      class="more-img"
+                    />
+                    <img
+                      src="/images/111079647.jpg"
+                      alt=""
+                      class="more-img"
+                    />
+                    <img
+                      src="/images/111079647.jpg"
+                      alt=""
+                      class="more-img"
+                    />
+                    <img
+                      src="/images/111079647.jpg"
+                      alt=""
+                      class="more-img"
+                    />
+                  </div>
+                  <p class="more-text">Lorem ipsum dolor sit amet.</p>
+                  <h2 class="more-subtitle">Lorem ipsum dolor sit amet.</h2>
+                  <p class="more-text">Lorem ipsum dolor sit amet.</p>
+                  <span class="more-bold">Lorem ipsum dolor sit amet.</span>
+                  <ul class="more-list">
+                    <li>Lorem, ipsum.</li>
+                    <li>Lorem, ipsum.</li>
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Lorem, ipsum dolor.</li>
+                  </ul>
+                  <div class="more-inner">
+                    <p class="more-inner-text">Lorem, ipsum dolor.</p>
+                  </div>
+                  <h2 class="more-subtitle">
+                    Lorem ipsum dolor sit amet consectetur.
+                  </h2>
+                  <p class="more-text">Lorem ipsum dolor sit amet.</p>
+                  <ol class="more-list">
+                    <li>Lorem, ipsum.</li>
+                    <li>Lorem, ipsum.</li>
+                    <li>Lorem, ipsum dolor.</li>
+                    <li>Lorem, ipsum dolor.</li>
+                  </ol>
+                  <span class="more-small">Источник:</span>
+                  <p class="more-text-end">Lorem ipsum dolor sit.</p>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
---
+++ front/src/store/module/request.module.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+import store from "../index";
+const url =
+        "https://newsapi.org/v2/everything?" +
+        "q=Apple&" +
+        "from=2023-01-14&" +
+        "sortBy=popularity&" +
+        "apiKey=a6ac34004ad844879ecb370fbb115b08";
+
+
+export default {
+  namespaced: true,
+  state() {
+    return {
+      requests: [],
+    };
+  },
+  mutations: {
+    setRequest(state, requests){
+      state.requests = requests
+      console.log(state.requests)
+  },
+  },
+  actions: {
+    async load({commit}) {
+         try{
+            const data = await axios.get(url)
+            const requests = data.data.articles
+            commit('setRequest', requests)
+         }catch(e){
+            console.log(e)
+         }
+    },
+    async loadOne({ commit, dispatch }, id) {
+      try{
+        const data = await axios.get(url)
+        const requests = data.data.articles[id]
+        commit('setRequest', requests)
+     }catch(e){
+        console.log(e)
+     }
+    },
+  },
+  getters: {
+    requests(state) {
+      return state.requests;
+    },
+  },
+};
